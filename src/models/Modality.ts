b6824a7bf3f3068849266e7c4b92b7ef98ba0925
Gagik <alixanyangagik@gmail.com>
2018-04-04 23:21:03 +0400

    added modality model

---
+++ src/models/Modality.ts
@@ -0,0 +1,36 @@
+'use strict';
+
+import {Schema, model} from 'mongoose';
+import * as mongoosePaginate from 'mongoose-paginate';
+
+const modalitySchema: Schema = new Schema({
+        type: {
+            type: String,
+            required: true,
+            enum: ['whole_class', 'groups', 'pairs', 'individual']
+        },
+        say: {
+            type: String,
+            required: true,
+        },
+        ask: {
+            type: String,
+            required: true
+        },
+        discuss: {
+            type: String,
+            required: true
+        }
+    },
+    {
+        versionKey: false,
+        timestamps: {
+            createdAt: 'created',
+            updatedAt: 'updated'
+        }
+    }
+);
+
+modalitySchema.plugin(mongoosePaginate);
+
+export default model('Modality', modalitySchema);
